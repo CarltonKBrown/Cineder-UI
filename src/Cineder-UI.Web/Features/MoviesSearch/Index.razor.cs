3be0eedc4fc76be8f1ea3b5fbc0224dd083ae196
Carlton Brown <carltonkbrown@outlook.com>
2024-03-19 18:57:29 -0500

    Refactored movie search page to use EditForm and a unified page model. Added margin top to footer.

--- src/Cineder-UI.Web/Features/MoviesSearch/Index.razor.cs
+++ src/Cineder-UI.Web/Features/MoviesSearch/Index.razor.cs
@@ -1,13 +1,20 @@
-﻿using Cineder_UI.Web.Interfaces.Store;
-using Cineder_UI.Web.Models.Api;
+﻿using Cineder_UI.Web.Features.MoviesSearch.Models;
+using Cineder_UI.Web.Interfaces.Store;
 using Cineder_UI.Web.Models.Common;
 using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
 
 namespace Cineder_UI.Web.Features.MoviesSearch
 {
     public partial class Index
     {
+        [Inject]
+        NavigationManager NavMngr { get; set; } = default!;
+
+        [Inject]
+        IStateContainer? Store { get; set; }
+
+        [SupplyParameterFromForm]
+        public MovieSearchPageModel PageModel { get; set; } = new();
 
         [Parameter]
         [SupplyParameterFromQuery]
@@ -17,96 +24,70 @@ namespace Cineder_UI.Web.Features.MoviesSearch
         [SupplyParameterFromQuery]
         public int Page { get; set; } = 1;
 
-        public string PageString  => Page.ToString();
+        public string PageString => Page.ToString();
 
-		public string TotalPagesString => MoviesResults!.TotalPages.ToString();
+        public string TotalPagesString => PageModel!.MoviesResults!.TotalPages.ToString();
 
-        [Inject]
-		NavigationManager? NavMngr { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            Store!.OnStateChanged += StateHasChanged;
 
-		[Inject]
-		IStateContainer? Store { get; set; }
+            await Store!.InitializeStore();
 
-		public MovieSearch? Model { get; set; } = new();
-		public MovieFilter? Filter { get; set; } = new();
-		public SearchResult<MoviesResult>? MoviesResults { get; set; } = new();
+            await SearchMovies();
 
-		public class MovieSearch
-		{
-            public string? Search { get; set; }
+            var movies = Store?.State.MovieState.SearchResult ?? new();
+
+            PageModel ??= new(SearchText, 0, movies);
+
+            await base.OnInitializedAsync();
         }
 
-		public class MovieFilter
-		{
-            public MovieFilter()
+        protected override void OnParametersSet()
+        {
+            if (SearchText != PageModel.Search)
             {
-				Selected = (int)SortOptions.None;
+                PageModel = new(SearchText, 0, Store!.State.MovieState.SearchResult);
             }
 
-            public int? Selected { get; set; } = (int)SortOptions.None; 
-		}
-
-		protected override async Task OnInitializedAsync()
-		{
-			Store!.OnStateChanged += StateHasChanged;
-
-			await Store!.InitializeStore();
-
-			await SearchMovies();
-
-			Model ??= new();
-
-			Filter ??= new();
-
-			MoviesResults  = Store?.State.MovieState.SearchResult ?? new();
-
-			await base.OnInitializedAsync();
-		}
-
-		protected override void OnParametersSet()
-		{
-			Model!.Search = Store!.State.MovieState.SearchText;
-
-			MoviesResults = Store!.State.MovieState.SearchResult;
-
-			base.OnParametersSet();
-		}
-
-		private static IEnumerable<FilterOption>? FilterOptions
-		{
-			get
-			{
-				var opt = Enum.GetValues<SortOptions>();
-
-				var finalList = opt.Select(FilterOption.FromSortOptions);
-
-				return finalList;
-			}
-		}
-
-		private async Task SearchMovies()
-		{
-			await Store!.SetMoviesSearch(SearchText, Page);
-		}
-
-		private async Task ChangePage(string pageNum)
-		{
-			if (!int.TryParse(pageNum, out var pageNumInt))
-			{
-				return;
-			}
-
-			if ((Store?.State?.MovieState?.SearchResult?.Page ?? 1) == pageNumInt)
-			{
-				return;
-			}
-
-			Page = pageNumInt;
-
-			await SearchMovies();
-
-			NavMngr.NavigateTo($"/movies?searchText={SearchText}&page={Page}");
+            base.OnParametersSet();
         }
 
-	}
+        private static IEnumerable<FilterOption>? FilterOptions
+        {
+            get
+            {
+                var opt = Enum.GetValues<SortOptions>();
+
+                var finalList = opt.Select(FilterOption.FromSortOptions);
+
+                return finalList;
+            }
+        }
+
+        private async Task SearchMovies()
+        {
+            await Store!.SetMoviesSearch(SearchText, Page);
+        }
+
+        private async Task ChangePage(string pageNum)
+        {
+            if (!int.TryParse(pageNum, out var pageNumInt))
+            {
+                return;
+            }
+
+            if ((Store?.State?.MovieState?.SearchResult?.Page ?? 1) == pageNumInt)
+            {
+                return;
+            }
+
+            Page = pageNumInt;
+
+            await SearchMovies();
+
+            NavMngr.NavigateTo($"/movies?searchText={SearchText}&page={Page}");
+        }
+
+    }
 }
---
+++ src/Cineder-UI.Web/Features/MoviesSearch/Models/MovieSearchPageModel.cs
@@ -0,0 +1,22 @@
+﻿using Cineder_UI.Web.Models.Api;
+using Cineder_UI.Web.Models.Common;
+
+namespace Cineder_UI.Web.Features.MoviesSearch.Models
+{
+    public class MovieSearchPageModel
+    {
+        public MovieSearchPageModel(string search, int selected, SearchResult<MoviesResult>? moviesResults)
+        {
+            Search = search;
+            Selected = selected;
+            MoviesResults = moviesResults;
+        }
+
+        public MovieSearchPageModel():this(string.Empty, 0, new()) { }
+
+        public string Search { get; set; } = string.Empty;
+        public int Selected { get; set; } = (int)SortOptions.None;
+        public SearchResult<MoviesResult>? MoviesResults { get; set; } = new();
+    }
+
+}
